aa105d60a41dc10ec06956e92b7948aad3f7c4cd
unknown <Wazzybtw@gmail.com>
2024-09-22 16:54:55 -0300

    comit

--- gameredge/src/components/App.jsx
+++ gameredge/src/components/App.jsx
@@ -1,11 +1,8 @@
-// src/App.js
-import React from "react";
-import ProductList from "./components/ProductList";
-import FilteredView from "./components/FilteredView";
 
 function App() {
   return (
     <>
+
     </>
   );
 }
