3b931e94b2ce0b83172e66930fe249a9fb5dd681
unknown <Wazzybtw@gmail.com>
2024-09-22 16:19:35 -0300

    comit

--- gameredge/src/components/App.jsx
+++ gameredge/src/components/App.jsx
@@ -5,10 +5,8 @@ import FilteredView from "./components/FilteredView";
 
 function App() {
   return (
-    <div className="App">
-      <ProductList />
-      <FilteredView />
-    </div>
+    <>
+    </>
   );
 }
 
